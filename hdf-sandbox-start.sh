0c63a36e978731e69d2187ade26d4f00cbe4663a
James <jmedel@hortonworks.com>
2017-06-09 09:05:34 -0700

    update hdf-sandbox-start.sh
    udpathe port forwarding to the hdf sandbox schema for communication with hdp sandbox

--- hdf-sandbox-start.sh
+++ hdf-sandbox-start.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 echo "Deploying HDF Sandbox Container:"
 until docker ps 2>&1| grep STATUS>/dev/null; do  sleep 1; done; >/dev/null
-docker ps -a | grep sandbox
+docker ps -a | grep sandbox-hdf
 if [ $? -eq 0 ]; then
- docker start sandbox
+ docker start sandbox-hdf
 else
-docker run --name sandbox --hostname "sandbox-hdf.hortonworks.com" --privileged -d \
+docker run --name sandbox-hdf --hostname "sandbox-hdf.hortonworks.com" --privileged -d \
 -p 12181:2181 \
--p 17010:16010 \
--p 51070:50070 \
 -p 13000:3000 \
 -p 14200:4200 \
 -p 14557:4557 \
@@ -22,34 +20,28 @@ docker run --name sandbox --hostname "sandbox-hdf.hortonworks.com" --privileged 
 -p 19000:9000 \
 -p 19090:9090 \
 -p 19091:9091 \
--p 42111:42111 \
+-p 43111:42111 \
 -p 62888:61888 \
--p 15100:15100 \
--p 15101:15101 \
--p 15102:15102 \
--p 15103:15103 \
--p 15104:15104 \
--p 15105:15105 \
+-p 25100:15100 \
+-p 25101:15101 \
+-p 25102:15102 \
+-p 25103:15103 \
+-p 25104:15104 \
+-p 25105:15105 \
 -p 17000:17000 \
 -p 17001:17001 \
 -p 17002:17002 \
 -p 17003:17003 \
 -p 17004:17004 \
 -p 17005:17005 \
--p 18081:8081 \
--p 18090:8090 \
--p 19060:9060 \
--p 19089:9089 \
--p 16667:6667 \
--p 17777:7777 \
--p 17788:7788 \
--p 17789:7789 \
 -p 12222:22 \
-sandbox-hdf /usr/sbin/sshd -D
+sandbox /usr/sbin/sshd -D
 fi
-docker exec -d sandbox service mysqld start
-docker exec -t sandbox service postgresql start
-docker exec -t sandbox service ambari-server start
-docker exec -t sandbox service ambari-agent start
+#docker exec -t sandbox /etc/init.d/startup_script start
+docker exec -d sandbox-hdf service mysqld start
+docker exec -d sandbox-hdf service ambari-server start
+docker exec -d sandbox-hdf service ambari-agent start
+docker exec -d sandbox-hdf /root/start_sandbox.sh
+docker exec -d sandbox-hdf /etc/init.d/tutorials start
 sleep 20
-echo "Successfully Started Sandbox Container"
+echo "Successfully Started HDF Sandbox Container"
